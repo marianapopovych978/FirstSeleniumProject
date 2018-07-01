79b25ec068ebf20c612debc80873abe95ce9632c
CybertekSchool <marianapopovych978@gmail.com>
2018-06-30 17:39:16 -0700

    goole

--- src/com/cbt/GoogleSearch.java
+++ src/com/cbt/GoogleSearch.java
@@ -1,10 +1,16 @@
 package com.cbt;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class GoogleSearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.gecko.driver",
+				"/Users/mariana/Documents/selenium dependencies/drivers/geckodriver 2");
+				WebDriver driver = new FirefoxDriver();
+				driver.get("https://google.com");
+				
 	}
 
 }
